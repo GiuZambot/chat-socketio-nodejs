c0a7e5b83c02ba1136e84cd5be95889ecb117e78
Giu Zambot <giuzambot@gmail.com>
2021-06-13 14:53:48 -0300

    Tentando de outra forma

---
+++ server.js
@@ -0,0 +1,27 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('./socket.io/dist/index')(http);
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/cli/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
